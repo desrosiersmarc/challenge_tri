189aa6b96196829db6e88eb9271834234e696f44
marc desrosiers <mdesrosiers@norauto.fr>
2019-11-18 20:54:31 +0100

    create clubs model, controller and index view

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,3 +1,4 @@
 class League < ApplicationRecord
   has_many :users
+  has_many :clubs
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :leagues, only: [:index]
   resources :formats, only: [:index]
+  resources :clubs, only: [:index]
 end
